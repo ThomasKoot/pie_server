c2d9744558459cce86d0db92e7b77598b8409383
Thomas Koot <info@thomaskoot.nl>
2020-10-16 10:21:55 +0200

    build v1

---
+++ build/precache-manifest.5b9ea4f3f0f0c0cee438d841d81c5ca4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36afefc1abe56ef20275893d7f5c25e4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4500f1fbadbdd0ee2b85",
+    "url": "/static/css/main.46f8760c.chunk.css"
+  },
+  {
+    "revision": "fc6737953f17b4935e26",
+    "url": "/static/js/2.5ec8e8bf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.5ec8e8bf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4500f1fbadbdd0ee2b85",
+    "url": "/static/js/main.b9c3fd84.chunk.js"
+  },
+  {
+    "revision": "c915a415783970d4a4a1",
+    "url": "/static/js/runtime-main.ddb61ef6.js"
+  }
+]);
